91e2db075d95d46e68ee48ff34733b20d3c4f24d
Paa Kwesi Owusu-Ansah <kwesta96@gmail.com>
2022-03-02 17:18:19 +0200

    new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import React from "react";
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 
 
 function App() {
   return (
-    <div className="app">
-      hello
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <div className="container">
+          <Sidebar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/users">
+              <UserList />
+            </Route>
+            <Route path="/user/:userId">
+              <User />
+            </Route>
+            <Route path="/newUser">
+              <NewUser />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
